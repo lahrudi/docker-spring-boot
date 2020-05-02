a4b6d93bf3d341efa5e1aa7a8b18a1f751dad66c
Alireza Gholamzadeh Lahroodi <AlirezaGholamzadehLahrudi@gmail.com>
2020-05-02 23:28:13 +0430

    Initial commit

---
+++ src/main/java/com/docker/HelloResources.java
@@ -0,0 +1,16 @@
+package com.docker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/docker/hello")
+public class HelloResources {
+    @GetMapping
+    public String hello()
+    {
+        return "Hello World";
+    }
+
+}
